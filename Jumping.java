79df9d791b08645475e7d47c033e757a09d20d88
Kaarthik M <kaarthik.m2023@vitstudent.ac.in>
2026-02-04 16:17:22 +0530

    Add Jumping class with main method and greeting
    Fix typo in main method declaration and add a greeting message.

---
+++ Jumping.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+
+class Jumping{
+      pulbic static void main(String[] args)
+      {
+          System.out.println("Hello Everyone. You are listening to VIT Community Radio 90.8FM!");
+      }  
+}
