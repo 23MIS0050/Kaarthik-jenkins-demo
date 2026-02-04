cb8565d44432ab7cda6756dc9c24147c1df3e837
Kaarthik M <kaarthik.m2023@vitstudent.ac.in>
2026-02-04 16:43:47 +0530

    Fix typo in main method declaration

--- Jumping.java
+++ Jumping.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 
 class Jumping{
-      pulbic static void main(String[] args)
+      public static void main(String[] args)
       {
           System.out.println("Hello Everyone. You are listening to VIT Community Radio 90.8FM!");
       }  
